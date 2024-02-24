9cef1cbbb6db3c84d0f3d88d4e5e47e2a0fa6889
Juan P. Isaza M <jisaza@vaovatravel.com>
2024-02-24 00:09:15 -0500

    feat(entries): adds entries script file

---
+++ entries.py
@@ -0,0 +1,19 @@
+import time
+
+def write_entries(filename):
+    while True:
+        # Get current date and time
+        current_datetime = time.strftime("%Y-%m-%d %H:%M:%S")
+
+        # Write current date and time to the file
+        with open(filename, 'a') as file:
+            file.write(current_datetime + '\n')
+
+        # Wait for 2 seconds
+        time.sleep(2)
+
+# File path
+filename = 'entries.txt'
+
+# Write current date and time every 2 seconds
+write_entries(filename)
